65349453c727220ad8af7ad8e4cdaa759c7c3c2b
Jhordy Gavinchu <dev@jhordyess.com>
2023-04-10 19:56:46 +0000

    Updated bundle config ⚒ Added page alias Added CNAME support

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -14,6 +14,7 @@ module.exports = {
     alias: {
       "@components": path.resolve(__dirname, "src", "components"),
       "@styles": path.resolve(__dirname, "src", "styles"),
+      "@pages": path.resolve(__dirname, "src", "pages"),
     },
   },
   module: {
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -4,6 +4,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CnameWebpackPlugin = require("cname-webpack-plugin");
 
 module.exports = {
   entry: path.join(__dirname, "src", "index.tsx"),
@@ -17,6 +18,7 @@ module.exports = {
     alias: {
       "@components": path.join(__dirname, "src", "components"),
       "@styles": path.join(__dirname, "src", "styles"),
+      "@pages": path.resolve(__dirname, "src", "pages"),
     },
   },
   module: {
@@ -41,6 +43,9 @@ module.exports = {
       filename: "[name].[contenthash].css",
     }),
     new CleanWebpackPlugin(),
+    new CnameWebpackPlugin({
+      domain: "datun.jhordyess.com",
+    }),
   ],
   optimization: {
     minimize: true,
