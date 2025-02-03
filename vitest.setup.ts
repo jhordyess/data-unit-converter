33365ca8e6902712348d1737ce6ac1546ae1e7f9
jhordyess <dev@jhordyess.com>
2025-02-03 17:16:24 -0400

    refactor: Update setup file reference in vite.config.ts and simplify vitest.setup.ts

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,11 +1,8 @@
-//tests/setup.js
 import { afterEach } from 'vitest'
 import { cleanup } from '@testing-library/react'
-import * as matchers from '@testing-library/jest-dom/matchers'
-import { expect } from 'vitest'
 
-// extends Vitest's expect method with methods from react-testing-library
-expect.extend(matchers)
+//extends Vitest's expect method with methods from react-testing-library
+import '@testing-library/jest-dom/vitest'
 
 // runs a cleanup after each test case (e.g. clearing jsdom)
 afterEach(() => {
