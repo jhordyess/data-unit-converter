e6eaacc35b276230d38aac5dca6f0d66decb7a63
jhordyess <dev@jhordyess.com>
2025-02-03 17:26:26 -0400

    refactor: Update comment to clarify jest-dom integration in vitest.setup.ts

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,7 +1,7 @@
 import { afterEach } from 'vitest'
 import { cleanup } from '@testing-library/react'
 
-//extends Vitest's expect method with methods from react-testing-library
+//extends Vitest's expect method with methods from jest-dom
 import '@testing-library/jest-dom/vitest'
 
 // runs a cleanup after each test case (e.g. clearing jsdom)
