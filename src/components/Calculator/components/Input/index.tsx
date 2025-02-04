ac336e56d5c3cb79ad91717874f3520e1625ecb7
jhordyess <dev@jhordyess.com>
2025-02-04 12:12:13 -0400

    refactor: Update Input component to use setValue prop for handling input changes

--- src/components/Calculator/components/Input/index.tsx
+++ src/components/Calculator/components/Input/index.tsx
@@ -1,39 +1,37 @@
 import './style.css'
 
-import { type ChangeEventHandler, useImperativeHandle, useState, forwardRef } from 'react'
+import { useImperativeHandle, useState, forwardRef } from 'react'
 
 interface InputProps {
   value: string
+  setValue: (value: string) => void
   symbol: string
-  handleChangeValue: ChangeEventHandler<HTMLInputElement>
 }
 
 export interface InputRef {
   animationOn: () => void
 }
 
-export const Input = forwardRef<InputRef, InputProps>(
-  ({ symbol, value, handleChangeValue }, ref) => {
-    const [animation, setAnimation] = useState(false)
+export const Input = forwardRef<InputRef, InputProps>(({ symbol, value, setValue }, ref) => {
+  const [animation, setAnimation] = useState(false)
 
-    const animationOff = () => {
-      setAnimation(false)
-    }
-
-    const animationOn = () => {
-      setAnimation(true)
-    }
-
-    useImperativeHandle(ref, () => ({
-      animationOn
-    }))
-
-    return (
-      <div className={animation ? 'bFXyQu animation' : 'bFXyQu'} onAnimationEnd={animationOff}>
-        <div>{symbol}</div>
-
-        <input type="text" value={value} onChange={handleChangeValue} />
-      </div>
-    )
+  const animationOff = () => {
+    setAnimation(false)
   }
-)
+
+  const animationOn = () => {
+    setAnimation(true)
+  }
+
+  useImperativeHandle(ref, () => ({
+    animationOn
+  }))
+
+  return (
+    <div className={animation ? 'bFXyQu animation' : 'bFXyQu'} onAnimationEnd={animationOff}>
+      <div>{symbol}</div>
+
+      <input type="text" value={value} onChange={e => setValue(e.target.value)} />
+    </div>
+  )
+})
